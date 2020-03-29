ee0951341af1f8f3d31377115e643a83b863d2aa
fukuiretu <fukuiretu@gmail.com>
2020-03-29 22:18:32 +0900

    微調整

--- src/Code.ts
+++ src/Code.ts
@@ -5,7 +5,13 @@ const AWS_KEY = properties.getProperty("AWS_KEY");
 const AWS_SECRET = properties.getProperty("AWS_SECRET");
 const BUCKET_NAME = properties.getProperty("BUCKET_NAME");
 
-function datasetgen(): void {
+function genDataset(): void {
+  const ok = Browser.msgBox('S3にデータセットをアップしますがよろしいですか??', Browser.Buttons.OK_CANCEL)
+  if (ok !== 'ok') {
+    Browser.msgBox("実行をキャンセルしました")
+    return
+  }
+
   const generator = new JSONGenerator()
   const data = generator.genAll()
   Logger.log(data)
@@ -17,4 +23,16 @@ function datasetgen(): void {
     Utilities.newBlob(data, 'text/json', 'UTF-8'),
     { logRequests:true }
   )
+  Browser.msgBox('S3にデータセットをアップしました')
+
+  return
+}
+
+function debugDataset(): void {
+  const generator = new JSONGenerator()
+  const data = generator.genAll()
+
+  Browser.msgBox(data)
+
+  return
 }
